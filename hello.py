abfd8fcff8f4795822d8e00e0d22932d1fe1c23e
Mitantsoaa <mitaranaivo@gmail.com>
2023-09-16 22:18:09 +0300

    Test des variable avec python

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
 print("Hello world !")
+if 5 == 5:
+    print('Tena izy')
+
+a = "Bonjour le monde"
+b = 5
+if a != b:
+    print('Different')
+#This is a comment in python
+"""
+This is a comment
+written in
+more than just one line
+"""
+x = str(3)  
+y = int(3)    
+z = float(3)
+print(type(z))
+print(x,y,z)
