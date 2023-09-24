01dbbaad145e3b97c36a7660cc5709876971cbb3
Mitantsoaa <mitaranaivo@gmail.com>
2023-09-24 22:09:53 +0300

    Fonction et boucle python

--- hello.py
+++ hello.py
@@ -1,1 +1,38 @@
 print("Hello world !")
+
+# commentaire en python
+print ('affichage en python')
+
+# declaration de variable
+x = 15
+print(x)
+
+# variable string
+y = 'Nom'
+print(y)
+
+# condition python
+if x > 0:
+    print('C\'est positif')
+else:
+    print('C\'est negatif')
+
+# les conditions logiques
+z = 20
+print(x+z)
+print(x-z)
+print(x*z)
+print(x/z)
+# fonction python
+
+def first_func():
+    print('Hello kitty')
+
+# boucle for
+fruits = ["akondro", "paiso", "paoma", "manga"]
+for fruit in fruits:
+    print(fruit)
+
+var_string = 'banane'
+for lettre in var_string:
+    print(lettre)
