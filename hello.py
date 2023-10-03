40d338a87e3e2ae62917df79accab36a08c1f033
Mitantsoaa <mitaranaivo@gmail.com>
2023-10-03 06:58:51 +0300

    List method python

--- hello.py
+++ hello.py
@@ -36,3 +36,32 @@ for fruit in fruits:
 var_string = 'banane'
 for lettre in var_string:
     print(lettre)
+
+# change list item
+fruits[2] = 'banana'
+
+fruits[1:3] = ['fraise', 'coco']
+
+print(fruits)
+
+# Add list item
+fruits.append('ketapotsy')
+print(fruits)
+
+fruits.insert(2,'voasary')
+print(fruits)
+
+# ralonger
+autre = ['omby', 'gana', 'atody']
+fruits.extend(autre)
+print(fruits)
+
+# remove list item
+fruits.remove('voasary')
+print(fruits)
+
+fruits.pop(2)
+print(fruits)
+
+del fruits[0]
+
